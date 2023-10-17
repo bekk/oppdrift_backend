52d3168fe1d38abb2017b46931686cee50908178
Thomas Oddsund <thomas.oddsund@bidbax.no>
2023-10-18 00:16:56 +0200

    Basis-prosjekt med devcontainer

---
+++ basis/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "no.bekk"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+kotlin {
+    jvmToolchain(8)
+}
---
+++ basis/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("Du kan bruke dette prosjektet som utgangspunkt for å løse kata-oppgavene.")
+    println("Lykke til!")
+}
